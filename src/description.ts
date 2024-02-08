a45b2e32a04df403c5b183dd7b8b36c19e0873e2
Claire Saba <claire.saba@coderpad.io>
2024-02-08 17:01:01 +0100

    refactor(constants): rename constant and function parameters

--- src/description.ts
+++ src/description.ts
@@ -1,21 +1,21 @@
-import { DEFAULT_LOCALE_DESCRIPTION } from './constants'
+import { DEFAULT_DESCRIPTION_LOCALE } from './constants'
 import { Descriptions, LocalizedJSONSchema } from './types'
 import { isNotEmpty } from './utils'
 
-export function getDescriptions (schema: LocalizedJSONSchema, localeDescription: string = DEFAULT_LOCALE_DESCRIPTION): Descriptions {
+export function getDescriptions (schema: LocalizedJSONSchema, descriptionLocale: string = DEFAULT_DESCRIPTION_LOCALE): Descriptions {
   const description = isNotEmpty(schema.description) ? schema.description : undefined
   const descriptions = schema.descriptions
 
   if (descriptions == null) {
-    return description != null ? { [localeDescription]: description } : {}
+    return description != null ? { [descriptionLocale]: description } : {}
   }
 
-  if (description != null && !Object.keys(descriptions).includes(localeDescription)) {
-    descriptions[localeDescription] = description
+  if (description != null && !Object.keys(descriptions).includes(descriptionLocale)) {
+    descriptions[descriptionLocale] = description
   }
   return descriptions
 }
 
-export function getLocalizedDescription (schema: LocalizedJSONSchema, locale: string, localeDescription?: string): string | undefined {
-  return getDescriptions(schema, localeDescription)[locale]
+export function getLocalizedDescription (schema: LocalizedJSONSchema, locale: string, descriptionLocale?: string): string | undefined {
+  return getDescriptions(schema, descriptionLocale)[locale]
 }
